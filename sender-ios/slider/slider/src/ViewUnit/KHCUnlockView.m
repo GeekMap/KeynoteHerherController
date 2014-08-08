a6721d5454f8b0dc926bdd2e6c69f845ad7d71c7
Jarron Shih <jarronshih@gmail.com>
2014-08-08 17:42:10 +0800

    close#14 :refactor unlock view UI

--- sender-ios/slider/slider/src/ViewUnit/KHCUnlockView.m
+++ sender-ios/slider/slider/src/ViewUnit/KHCUnlockView.m
@@ -10,6 +10,7 @@
 
 #define ViewWidth 320
 #define ViewHeight 568
+#define ViewMask 0.85
 
 #define LabelOffsetX 10
 #define LabelWidth 20
@@ -18,12 +19,14 @@
 #define VolumeDownLabelPosX (LabelOffsetX)
 #define VolumeDownLabelPosY 142
 #define SliderTipLabelPosX 100
-#define SliderTipLabelPosY 450
+#define SliderTipLabelPosY 500
 
-#define SliderPosX 250
-#define SliderPosY 180
 #define SliderLength 350
-#define SlideWidth 50
+#define SliderWidth 95
+#define SliderPosX (320-SliderWidth)/2
+#define SliderPosY 150
+
+
 
 @interface KHCUnlockView()
 @property(nonatomic, strong) UIWindow *window;
@@ -32,48 +35,66 @@
 
 
 @implementation KHCUnlockView
-
+{
+    UILabel *sliderTipLabel;
+    UILabel *volumeUpLabel;
+    UILabel *volumeDownLabel;
+    BOOL did_unlock;
+}
 @synthesize window;
 - (id) init
 {
-    
+    did_unlock = NO;
     self = [super initWithFrame:[[UIScreen mainScreen] bounds]];
     if (self) {
-        self.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.8f];
+        self.backgroundColor = [UIColor colorWithWhite:0.0f alpha:ViewMask];
         // add Text
-        UILabel *volumeUpLabel = [[UILabel alloc] initWithFrame:CGRectMake(VolumeUpLabelPosX, VolumeUpLabelPosY, ViewWidth, LabelWidth)];
-        [volumeUpLabel setText:@"<-- Press to go UP"];
-        [volumeUpLabel setTextColor:[UIColor yellowColor]];
+        volumeUpLabel = [[UILabel alloc] initWithFrame:CGRectMake(VolumeUpLabelPosX, VolumeUpLabelPosY, ViewWidth, LabelWidth)];
+        [volumeUpLabel setText:@"<<<  Previous"];
+        [volumeUpLabel setTextColor:[UIColor lightGrayColor]];
         [volumeUpLabel setTextAlignment:NSTextAlignmentLeft];
-        [volumeUpLabel setFont:[UIFont boldSystemFontOfSize:15.0]];
+        [volumeUpLabel setFont:[UIFont boldSystemFontOfSize:20.0]];
         [self addSubview:volumeUpLabel];
         
-        UILabel *volumeDownLabel = [[UILabel alloc] initWithFrame:CGRectMake(VolumeDownLabelPosX, VolumeDownLabelPosY, ViewWidth, LabelWidth)];
-        [volumeDownLabel setText:@"<-- Press to go NEXT"];
-        [volumeDownLabel setTextColor:[UIColor yellowColor]];
+        volumeDownLabel = [[UILabel alloc] initWithFrame:CGRectMake(VolumeDownLabelPosX, VolumeDownLabelPosY, ViewWidth, LabelWidth)];
+        [volumeDownLabel setText:@"<<<  Next"];
+        [volumeDownLabel setTextColor:[UIColor lightGrayColor]];
         [volumeDownLabel setTextAlignment:NSTextAlignmentLeft];
-        [volumeDownLabel setFont:[UIFont boldSystemFontOfSize:15.0]];
+        [volumeDownLabel setFont:[UIFont boldSystemFontOfSize:20.0]];
         [self addSubview:volumeDownLabel];
         
-        UILabel *sliderTipLabel = [[UILabel alloc] initWithFrame:CGRectMake(SliderTipLabelPosX, SliderTipLabelPosY, ViewWidth, LabelWidth)];
-        [sliderTipLabel setText:@"Slide to UNLOCK -->"];
-        [sliderTipLabel setTextColor:[UIColor yellowColor]];
-        [sliderTipLabel setTextAlignment:NSTextAlignmentLeft];
-        [sliderTipLabel setFont:[UIFont boldSystemFontOfSize:15.0]];
+        sliderTipLabel = [[UILabel alloc] init];
+        sliderTipLabel.transform = CGAffineTransformMakeRotation(M_PI_2);
+        [sliderTipLabel setAutoresizesSubviews:NO];
+        [sliderTipLabel setFrame: CGRectMake(SliderPosX, SliderPosY, SliderWidth, SliderLength)];
+        [sliderTipLabel.layer setBackgroundColor:[UIColor grayColor].CGColor];
+        sliderTipLabel.layer.cornerRadius = SliderWidth/2;
+        [sliderTipLabel setText:@"slide to unlock\t\t"];
+        [sliderTipLabel setTextColor:[UIColor blackColor]];
+        [sliderTipLabel setTextAlignment:NSTextAlignmentRight];
+        [sliderTipLabel setFont:[UIFont boldSystemFontOfSize: 28.0]];
         [self addSubview:sliderTipLabel];
         
         
         // add Slide
         UISlider *slider = [[UISlider alloc] init];
         slider.transform = CGAffineTransformMakeRotation(M_PI_2);
-        [slider setFrame:CGRectMake(SliderPosX,SliderPosY,SlideWidth,SliderLength)];
+        [slider setFrame:CGRectMake(SliderPosX,SliderPosY,SliderWidth,SliderLength)];
         [slider addTarget:self action:@selector(sliderMoveAction:) forControlEvents:UIControlEventValueChanged];
         [slider addTarget:self action:@selector(sliderReset:) forControlEvents:UIControlEventTouchUpInside];
         [slider addTarget:self action:@selector(sliderReset:) forControlEvents:UIControlEventTouchUpOutside];
         [slider setBackgroundColor:[UIColor clearColor]];
         slider.minimumValue = 0.0f;
-        slider.maximumValue = 1.0f;
+        slider.maximumValue = SliderLength-SliderWidth;
         slider.value = 0.0f;
+        UIImage *stetchLeftTrack = [[UIImage imageNamed:@"Nothing.png"]
+                                   stretchableImageWithLeftCapWidth:30.0 topCapHeight:0.0];
+        UIImage *stetchRightTrack = [[UIImage imageNamed:@"Nothing.png"]
+                                    stretchableImageWithLeftCapWidth:30.0 topCapHeight:0.0];
+        UIImage *thumbImage = [UIImage imageNamed:@"slider-arrow.png"];
+        [slider setThumbImage:thumbImage  forState:UIControlStateNormal];
+        [slider setMinimumTrackImage:stetchLeftTrack forState:UIControlStateNormal];
+        [slider setMaximumTrackImage:stetchRightTrack forState:UIControlStateNormal];
         [slider setContinuous: YES];
         [slider setEnabled:YES];
         [slider setUserInteractionEnabled:YES];
@@ -101,6 +122,8 @@
 
 - (void) hide
 {
+    did_unlock = YES;
+    
     [UIView animateWithDuration:0.5f
                           delay:0.f
                         options:UIViewAnimationOptionTransitionCrossDissolve
@@ -117,8 +140,26 @@
 - (void) sliderMoveAction:(id)sender
 {
     UISlider *slider = (UISlider*)sender;
-    if (slider.value == slider.maximumValue)
+    if (slider.value >= slider.maximumValue)
+    {
         [self hide];
+    }
+    else if(did_unlock == NO)
+    {
+        volumeDownLabel.hidden = YES;
+        volumeUpLabel.hidden = YES;
+        [sliderTipLabel setFrame: CGRectMake(SliderPosX, SliderPosY+slider.value, SliderWidth, SliderLength-slider.value)];
+        float alpha = ViewMask - ViewMask*0.8*(slider.value/slider.maximumValue);
+        self.backgroundColor = [UIColor colorWithWhite:0.0f alpha:alpha];
+        if (SliderLength-slider.value > 300)
+        {
+            sliderTipLabel.text = @"slide to unlock\t\t";
+        }
+        else
+        {
+            sliderTipLabel.text = @"";
+        }
+    }
 }
 
 - (void) sliderReset:(id)sender
@@ -129,6 +170,12 @@
         [UIView animateWithDuration:0.2f
                          animations:^{
                              [slider setValue:0.0f animated:YES];
+                             [sliderTipLabel setFrame: CGRectMake(SliderPosX, SliderPosY+slider.value, SliderWidth, SliderLength)];
+                             self.backgroundColor = [UIColor colorWithWhite:0.0f alpha:ViewMask];
+                         } completion:^(BOOL finish){
+                            sliderTipLabel.text = @"slide to unlock\t\t";
+                            volumeDownLabel.hidden = NO;
+                            volumeUpLabel.hidden = NO;
                          }];
     }
 }
